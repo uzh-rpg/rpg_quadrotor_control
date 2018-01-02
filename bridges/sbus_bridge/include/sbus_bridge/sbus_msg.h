f1606cb3b9f0ace3ec3c8176e8200b3c3fda79ad
mfaessle <matfaessler@gmail.com>
2018-01-02 10:23:19 +0100

    turned sbus msg type into struct

--- bridges/sbus_bridge/include/sbus_bridge/sbus_msg.h
+++ bridges/sbus_bridge/include/sbus_bridge/sbus_msg.h
@@ -17,9 +17,27 @@ enum class ArmState
   DISARMED, ARMED
 };
 
-class SBusMsg
+struct SBusMsg
 {
-public:
+  // Constants
+  static constexpr int kNChannels = 16;
+  static constexpr uint16_t kMinCmd = 192; // corresponds to 1000 on FC
+  static constexpr uint16_t kMeanCmd = 992; // corresponds to 1500 on FC
+  static constexpr uint16_t kMaxCmd = 1792; // corresponds to 2000 on FC
+
+  ros::Time timestamp;
+
+  // Normal 11 bit channels
+  uint16_t channels[kNChannels];
+
+  // Digital channels (ch17 and ch18)
+  bool digital_channel_1;
+  bool digital_channel_2;
+
+  // Flags
+  bool frame_lost;
+  bool failsafe;
+
   SBusMsg();
   SBusMsg(const sbus_bridge::SbusRosMessage& sbus_ros_msg);
   ~SBusMsg();
@@ -45,25 +63,6 @@ public:
   // Sbus message check helpers
   bool isArmed() const;
   ControlMode getControlMode() const;
-
-  // Constants
-  static constexpr int kNChannels = 16;
-  static constexpr uint16_t kMinCmd = 192; // corresponds to 1000 on FC
-  static constexpr uint16_t kMeanCmd = 992; // corresponds to 1500 on FC
-  static constexpr uint16_t kMaxCmd = 1792; // corresponds to 2000 on FC
-
-  ros::Time timestamp;
-
-  // Normal 11 bit channels
-  uint16_t channels[kNChannels];
-
-  // Digital channels (ch17 and ch18)
-  bool digital_channel_1;
-  bool digital_channel_2;
-
-  // Flags
-  bool frame_lost;
-  bool failsafe;
 };
 
 } // namespace sbus_bridge
