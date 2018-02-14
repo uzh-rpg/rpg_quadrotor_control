04861cbeafff6f4f7a06508e85834a5ec3660be1
mfaessle <matfaessler@gmail.com>
2018-02-14 14:24:45 +0100

    implemented manual flight assistant

---
+++ utils/manual_flight_assistant/include/manual_flight_assistant/joypad_axes_buttons.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace manual_flight_assistant
+{
+
+namespace joypad
+{
+
+namespace axes
+{
+static constexpr uint32_t kX = 4;
+static constexpr uint32_t kY = 3;
+static constexpr uint32_t kZ = 1;
+static constexpr uint32_t kYaw = 0;
+} // namespace axes
+
+namespace buttons
+{
+static constexpr uint32_t kGreen = 0;
+static constexpr uint32_t kRed = 1;
+static constexpr uint32_t kBlue = 2;
+static constexpr uint32_t kYellow = 3;
+
+static constexpr uint32_t kLb = 4;
+static constexpr uint32_t kRb = 5;
+} // namespace buttons
+
+} // namespace joypad
+
+} // namespace manual_flight_assistant
