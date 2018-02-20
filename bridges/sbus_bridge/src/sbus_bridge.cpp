a53b136b0aeef845c59a0ef8c9802bbc0e226456
mfaessle <matfaessler@gmail.com>
2018-02-20 15:06:06 +0100

    added eigen dependency

--- bridges/sbus_bridge/src/sbus_bridge.cpp
+++ bridges/sbus_bridge/src/sbus_bridge.cpp
@@ -1,5 +1,6 @@
 #include "sbus_bridge/sbus_bridge.h"
 
+#include <Eigen/Dense>
 #include <quad_common/geometry_eigen_conversions.h>
 #include <quad_common/math_common.h>
 #include <quad_common/parameter_helper.h>
