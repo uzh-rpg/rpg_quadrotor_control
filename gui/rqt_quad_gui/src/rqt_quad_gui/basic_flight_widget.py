920336c9a2cb1e1281b78287de86a23577e4e68f
Julien Kohler <kohlerj@users.noreply.github.com>
2019-02-20 15:45:08 +0100

    Second column with camera exposure control

--- gui/rqt_quad_gui/src/rqt_quad_gui/basic_flight_widget.py
+++ gui/rqt_quad_gui/src/rqt_quad_gui/basic_flight_widget.py
@@ -3,12 +3,15 @@ from .quad_widget_common import QuadWidgetCommon
 
 from .autopilot_widget import AutopilotWidget
 
+from .cam_ctrl_widget import CameraControlWidget
 
 class BasicFlightWidget(QuadWidgetCommon):
     def __init__(self):
         super(BasicFlightWidget, self).__init__()
 
         self._column_1.addWidget(self._name_widget)
-        self._column_1.addWidget(AutopilotWidget(self))
+        self._column_1.addWidget( AutopilotWidget(self) )
+        
+        self._column_2.addWidget( CameraControlWidget(self) )
 
         self.setup_gui(two_columns=False)
